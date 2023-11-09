c8d77ca04f3b3e6ed160eb0e9e9c85429b08e2e2
unicornsystum <142894471+unicornsystum@users.noreply.github.com>
2023-11-10 00:35:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './componants/style.css'
 
 function App() {
+  const [action, setAction] = useState('Login');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <div className='header'>
+          <div className='text'>{action}</div>
+          <div className='underline'></div>
+        </div>
+        <div className='inputs'>
+          {action === "Login" ? <div></div> : <div className='input'>
+            <i class="fa-solid fa-user"></i>
+            <input type='text' name='' placeholder='Name' />
+          </div>}
+          <div className='input'>
+            <i class="fa-solid fa-envelope"></i>
+            <input type='email' name='' placeholder='Email Id' />
+          </div>
+          <div className='input'>
+            <i class="fa-solid fa-lock"></i>
+            <input type='password' name='' placeholder='password' />
+          </div>
+          {action ==="Sing Up"?<div></div>:<div className='forgot-password'>
+            Lost Password? <span>Click Here</span>
+          </div>}
+          
+          
+
+          <div className='submit-container'>
+            <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction("Sing Up") }}>Sing Up</div>
+            <div className={action === "Sing Up" ? "submit gray" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
+          </div>
+
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
